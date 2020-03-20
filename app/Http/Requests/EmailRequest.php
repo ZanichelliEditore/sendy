7cce3ce31103b58a04f80f1b79368d5e73e041af
gsaraceno <giuseppe.saraceno@zanichelli.it>
2020-03-20 10:36:34 +0100

    add max length validation

--- app/Http/Requests/EmailRequest.php
+++ app/Http/Requests/EmailRequest.php
@@ -26,11 +26,11 @@ class EmailRequest extends FormRequest
         $formatEmail = 'regex:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
         return [
             'to' => 'required|array|min:1',
-            'to.*' => ['required', 'email', $formatEmail],
+            'to.*' => ['required', 'email', $formatEmail, 'max:320'],
             'cc' => 'sometimes|array|nullable',
-            'cc.*' => ['email', $formatEmail],
+            'cc.*' => ['email', $formatEmail, 'max:320'],
             'bcc' => 'sometimes|array|nullable',
-            'bcc.*' => ['email', $formatEmail],
+            'bcc.*' => ['email', $formatEmail, 'max:320'],
             'from' => 'required|email',
             'subject' => 'sometimes|string|max:100|nullable',
             'body' => 'sometimes|string|nullable',
