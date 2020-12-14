c9ec89af368f844db5893d3dc6b15e9023ce3cc1
Michele Salvador <michele.salvador@absontheweb.com>
2020-12-14 15:36:52 +0100

    MLPR-64 improved gate to allow ADMIN IDP only access

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
@@ -21,15 +22,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
-            }
-
-            return $entry->isReportableException() ||
-                $entry->isFailedRequest() ||
-                $entry->isFailedJob() ||
-                $entry->isScheduledTask() ||
-                $entry->hasMonitoredTag();
+            return true;
         });
     }
 
@@ -63,7 +56,12 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array('VIEW_TELESCOPE', $user->permissions);
+            $roles = [];
+            foreach($user->roles as $role) {
+                $roles[] = $role->roleName;
+            }
+            $roles = array_unique($roles);
+            return in_array("ADMIN_IDP", $roles);
         });
     }
 }
