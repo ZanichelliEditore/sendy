7448af855479f71c821e59eb75a531497bffc246
Axl Fernandez <a.fernandez@esis-italia.com>
2021-02-08 17:42:53 +0100

    SND-9 laravel 6

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Jenssegers\Mongodb\MongodbServiceProvider::class,
+        DesignMyNight\Mongodb\MongodbPassportServiceProvider::class,
 
         /*
          * Package Service Providers...
