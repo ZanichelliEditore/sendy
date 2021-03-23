6ed353cfdc80874d3ff729976d854f674b987f08
Michele Salvador <michele.salvador@absontheweb.com>
2021-03-23 15:10:35 +0100

    SND-12 added unauthorized view and route

---
+++ resources/views/unauthorized.blade.php
@@ -0,0 +1,39 @@
+@extends('app')
+
+@section('body')
+<div class="mx-5">
+    <div class="dialog-unauthorized">
+        <div class="unauthorized">
+            NON SEI AUTORIZZATO
+        </div>
+        <br>
+        <br>
+        <div class="box-lemma">
+            <h1 class="lemma">Autorizzato:</h1>
+            <h2 class="verb">
+                part. pass. di autorizzare; anche agg.
+            </h2>
+            <br/>
+            <h2 class="definition">
+                <ul>
+                    <li>
+                        <p>
+                            che è permesso, consentito: manifestazione non autorizzata
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            approvato, riconosciuto ufficialmente: traduzione autorizzata; biografia non autorizzata | che ha il diritto o la facoltà di fare qlco.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            (+a seguito da inf.): non sono autorizzato a fornire informazioni.
+                        </p>
+                    </li>
+                </ul>
+            </h2>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'idp'], function () {
-    Route::get('/', function () {
-        return redirect('failedJobs');
-    });
-    
-    Route::get('/failedJobs', function () {
-        return view('failedJobs');
-    })->name('failedJobs');
 
-    Route::get('/jobs', function () {
-        return view('jobs');
-    })->name('jobs');
+    Route::get('/unauthorized', function () {
+        return view('unauthorized');
+    })->name('unauthorized');
+
     Route::get('logout',  'LoginController@logout');
+
+    Route::group(['middleware' => 'check.role'], function () {
+
+        Route::get('/', function () {
+            return redirect('failedJobs');
+        });
+
+        Route::get('/failedJobs', function () {
+            return view('failedJobs');
+        })->name('failedJobs');
+
+        Route::get('/jobs', function () {
+            return view('jobs');
+        })->name('jobs');
+    });
 });
