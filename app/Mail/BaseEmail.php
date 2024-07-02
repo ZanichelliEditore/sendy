1e68676f6a67232760185825dffa3b08765c03d4
alicegambella <alice.gambella@absontheweb.com>
2024-07-02 12:47:13 +0200

    SND-27: add BaseEmail update

--- app/Mail/BaseEmail.php
+++ app/Mail/BaseEmail.php
@@ -2,7 +2,6 @@
 
 namespace App\Mail;
 
-
 use App\Models\Email;
 use Illuminate\Mail\Mailable;
 use Illuminate\Support\Facades\Storage;
@@ -10,10 +9,14 @@ use Illuminate\Support\Facades\Storage;
 abstract class BaseEmail extends Mailable
 {
     protected $email;
+    protected $size;
+
+    private $storage;
 
     public function __construct(Email $email)
     {
         $this->email = $email;
+        $this->storage = Storage::disk('local');
     }
 
     /**
@@ -29,9 +32,11 @@ abstract class BaseEmail extends Mailable
             ->bcc($this->email->getBcc())
             ->subject($this->email->getSubject())
             ->view($this->useView())
-            ->with([
-                'body' => $this->email->getBody()
-            ]);
+            ->with(
+                [
+                    'body' => $this->email->getBody()
+                ]
+            );
 
         $directory = $this->email->getAttachmentsDirectory();
 
@@ -44,6 +49,23 @@ abstract class BaseEmail extends Mailable
         return $builder;
     }
 
+    public function saveAttachments($attachments)
+    {
+        if (!$this->size) $this->calcSize();
+
+        $directory = $this->email->getAttachmentsDirectory();
+        foreach ($attachments as $file) {
+            $this->size += $file->getSize();
+            Storage::putFileAs('attachments/' . $directory, $file, $file->getClientOriginalName());
+        }
+    }
+
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
     /**
      * Get email to send
      *
@@ -54,10 +76,67 @@ abstract class BaseEmail extends Mailable
         return $this->email;
     }
 
+
+    public function calcSize(): int
+    {
+        $directory = $this->email->getAttachmentsDirectory();
+
+        $headers = array(
+            'from' => $this->email->getFrom(),
+            'sender' => $this->email->getSender(),
+            'to' => $this->email->getTo(),
+            'cc' => $this->email->getCc(),
+            'bcc' => $this->email->getBcc(),
+            'subject' => $this->email->getSubject()
+        );
+
+        $body = $this->email->getBody();
+        $size = strlen($body);
+
+        if ($imgesSize = $this->getImagesSize($body)) $size += $imgesSize;
+
+        foreach ($headers as $header) {
+            if (is_array($header)) $size += strlen(implode('', $header));
+            else $size += strlen($header);
+        }
+
+        if ($directory) {
+            foreach (Storage::files('attachments/' . $directory, true) as $attach) {
+                $size += $this->storage->size($attach);
+            }
+        }
+        return $this->size = $size;
+    }
+
     /**
      * Returns the view to use.
      *
      * @return string
      */
     protected abstract function useView();
+
+
+    private function getImagesSize(string $body): int|null
+    {
+        preg_match_all('/(<img)[[:word:][:punct:][:space:]]*(src=")[[:word:][:punct:]]*"/m', $body, $matches);
+
+        if (empty($matches)) return null;
+
+        $size = 0;
+        foreach ($matches[0] as $match) {
+            $filename = str_replace('"', '', explode('src="', $match))[1];
+
+            //INFO: get image size info
+            extract(
+                getimagesize($filename),
+                EXTR_PREFIX_INVALID,
+                'dimension'
+            );
+            var_dump($dimension_0 * $dimension_1 * $bits);
+
+            //INFO: size = (width * height * BPP) / bit per byte
+            $size += ($dimension_0 * $dimension_1 * $bits) / 8;
+        }
+        return $size;
+    }
 }
