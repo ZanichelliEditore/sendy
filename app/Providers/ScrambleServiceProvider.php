f37abb79f74cde4eda705751ca28a57abd05ea74
Veronica Vannini <vvannini@zanichelli.it>
2026-09-25 15:36:58 +0200

    MLPR-612: remove unused code

--- app/Providers/ScrambleServiceProvider.php
+++ app/Providers/ScrambleServiceProvider.php
@@ -23,32 +23,6 @@ class ScrambleServiceProvider extends ServiceProvider
       ->expose(
         ui: '/api/documentation',
         document: '/docs/api.json',
-      )
-      ->withDocumentTransformers(function (OpenApi $openApi) {
-
-        // INFO: define security schemas
-        $openApi->secure(SecurityScheme::http('basic')->as("basicAuth"));
-        $openApi->secure(SecurityScheme::oauth2()
-          ->as("passport")
-          ->flow('clientCredentials', function (OAuthFlow $flow) {
-            $flow
-              ->tokenUrl(config('app.url') . '/oauth/token')
-              ->addScope('*', 'all');
-          }));
-      })
-
-      ->withOperationTransformers(function (Operation $operation, RouteInfo $routeInfo) {
-
-        // INFO: assign security schema based on middleware
-        $routeMiddlewares = collect($routeInfo->route->gatherMiddleware());
-        if ($routeMiddlewares->contains("basicAuth")) {
-          $operation->addSecurity(new SecurityRequirement(["basicAuth" => []]));
-        } elseif ($routeMiddlewares->contains("client")) {
-          $operation->addSecurity(new SecurityRequirement(["passport" => []]));
-        } else {
-          $operation->security = [];
-        }
-      })
-    ;
+      );
   }
 }
