1f935ac7f87188b950d8c362872ffee4d8820d58
Veronica Vannini <vvannini@zanichelli.it>
2026-09-25 16:26:06 +0200

    MLPR-612: remove unused imports

--- app/Providers/ScrambleServiceProvider.php
+++ app/Providers/ScrambleServiceProvider.php
@@ -4,14 +4,10 @@ namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
-use Dedoc\Scramble\Support\Generator\Schema;
 use Dedoc\Scramble\Support\Generator\SecurityRequirement;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Dedoc\Scramble\Support\Generator\SecuritySchemes\OAuthFlow;
-use Dedoc\Scramble\Support\Generator\Types\StringType;
 use Dedoc\Scramble\Support\Generator\Operation;
-use Dedoc\Scramble\Support\Generator\Parameter;
-use Dedoc\Scramble\Support\Generator\RequestBodyObject;
 use Dedoc\Scramble\Support\RouteInfo;
 use Illuminate\Support\ServiceProvider;
 
